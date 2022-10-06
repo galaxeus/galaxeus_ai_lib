b542ce6725359cc8159a76b066a9c09c3f7500f0
Your Name <you@example.com>
2022-10-06 17:22:03 +0700

    update

---
+++ src/galaxeus_ai_dart/bin/galaxeus_ai_dart.dart
@@ -0,0 +1,5 @@
+import 'package:galaxeus_ai_dart/galaxeus_ai_dart.dart' as galaxeus_ai_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${galaxeus_ai_dart.calculate()}!');
+}
---
+++ src/galaxeus_ai_dart/test/galaxeus_ai_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:galaxeus_ai_dart/galaxeus_ai_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
