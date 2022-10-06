77c10d7eb137c8225b028784b725b45d461b3b18
Your Name <you@example.com>
2022-10-06 22:15:49 +0700

    update

--- src/galaxeus_ai_dart/example/qr_code/qr_code.dart
+++ src/galaxeus_ai_dart/example/qr_code/qr_code.dart
@@ -6,7 +6,7 @@ void main(List<String> args) async {
   GalaxeusQrCode galaxeusQrCode = GalaxeusQrCode();
   File fileOutPut = File("./data.png");
   File encode = await galaxeusQrCode.encode(text: "Hai ini text qr", fileOutPut: fileOutPut, padding: 10);
-  var image = img.decodePng(fileOutPut.readAsBytesSync())!;
+  var image = img.decodePng(encode.readAsBytesSync())!;
 
   var result = galaxeusQrCode.decode(image: image);
   print(result);
