f9645181da4b16526b41086ba17840493f6b7e0a
Your Name <you@example.com>
2022-10-06 18:04:09 +0700

    update

--- src/galaxeus_ai_dart/lib/src/scheme/galaxeus_ai_request.dart
+++ src/galaxeus_ai_dart/lib/src/scheme/galaxeus_ai_request.dart
@@ -3,7 +3,13 @@ part of galaxeus_ai_dart;
 class GalaxeusAiRequest {
   late Map rawData;
   GalaxeusAiRequest(this.rawData);
- 
+
+  factory GalaxeusAiRequest.chatbot({
+    required Map from,
+  }) {
+    return GalaxeusAiRequest({});
+  }
+
   Map toMap() {
     return (rawData);
   }
