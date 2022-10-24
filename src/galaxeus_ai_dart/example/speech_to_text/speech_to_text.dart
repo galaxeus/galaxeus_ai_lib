fbd6f5764d56372c9c6cda2469a93e317b5d740c
Your Name <you@example.com>
2022-10-24 16:37:30 +0700

    update

--- src/galaxeus_ai_dart/example/speech_to_text/speech_to_text.dart
+++ src/galaxeus_ai_dart/example/speech_to_text/speech_to_text.dart
@@ -2,7 +2,7 @@ import 'package:galaxeus_ai_dart/galaxeus_ai_dart.dart';
 import 'package:galaxeus_lib/galaxeus_lib.dart';
 import 'package:universal_io/io.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   DateTime time = DateTime.now();
   GalaxeusAiNative galaxeusAiNative = GalaxeusAiNative(
     galaxeusAiLib: "../../native_lib/galaxeus_ai.so",
@@ -15,16 +15,20 @@ void main(List<String> args) {
     ),
   );
 
-  GalaxeusAiNativeResponse res = galaxeusAiNative.request(
-    galaxeusAiNativeRequest: GalaxeusAiNativeRequest.speechToTextFromWavFile(
-      // audio: GalaxeusAiAudioConvert.convertToWav16BitSync(
-      //   audioInput: File("../../native_lib/samples/audio.wav"),
-      //   audioOutput: File("../../native_lib/samples/output_res.wav"),
-      // ),
-      audio: File("../../native_lib/samples/output_res.wav"),
-      model: File("../../native_lib/models/ggml-model-whisper-small.bin"),
-    ),
-  );
-  print(res.toString());
+  Future.microtask(() {
+    GalaxeusAiNativeResponse res = galaxeusAiNative.request(
+      galaxeusAiNativeRequest: GalaxeusAiNativeRequest.speechToTextFromWavFile(
+        // audio: GalaxeusAiAudioConvert.convertToWav16BitSync(
+        //   audioInput: File("../../native_lib/samples/audio.wav"),
+        //   audioOutput: File("../../native_lib/samples/output_res.wav"),
+        // ),
+        audio: File("../../native_lib/samples/output_res.wav"),
+        model: File("../../native_lib/models/ggml-model-whisper-small.bin"),
+      ),
+    );
+    print(res.toString());
+    print(convertToAgoFromDateTime(time));
+    return;
+  });
   print(convertToAgoFromDateTime(time));
 }
